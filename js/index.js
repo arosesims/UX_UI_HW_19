d5867492f05c00b5c960062e4cf03778039a1bf5
arosesims <78513699+arosesims@users.noreply.github.com>
2024-05-02 15:03:28 -0500

    Ripple Button

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
-// works better in Css, make a new JS translation for this 
+// Skill hover animations are in CSs 
 
-//$(".flexItem").hover(function () {
-// only moves once
-// make a smoother transformation  mate it return to where it was
-//$('.flexItem').css({ "transform": "translate(20px)", "transition": "transform .5 ease" });
-//}); 
+// Ripple Button Animation 
+// button animation refernced from https://www.sitepoint.com/button-micro-interactions/#ripplemicrointeraction
+
+
+let btn = document.querySelectorAll(".rippleButton");
+btn.forEach((btn) => {
+    btn.onclick = function (e) {
+        let x = e.clientX;
+        let y = e.pageY;
+        let ripples = document.createElement("span");
+
+        ripples.style.left = x + "px";
+        ripples.style.top = y + "px";
+        this.appendChild(ripples);
+
+        setTimeout(() => {
+            ripples.remove();
+        }, 1500); /*1000 = 1s*/
+    };
+});
