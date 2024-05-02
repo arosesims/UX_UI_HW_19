90bace7b8cc011dc00cab7f749a66758ad8dc543
arosesims <78513699+arosesims@users.noreply.github.com>
2024-05-02 13:49:03 -0500

    Skill Hover Interaction Save

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// works better in Css, make a new JS translation for this 
+
+//$(".flexItem").hover(function () {
+// only moves once
+// make a smoother transformation  mate it return to where it was
+//$('.flexItem').css({ "transform": "translate(20px)", "transition": "transform .5 ease" });
+//}); 
